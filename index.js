4ea670668975546b2f69f6c2f0d38dd824418d7a
schrock <schrock@users.noreply.github.com>
2017-02-08 20:52:34 -0800

    first stab at listing dir contents

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-	var files = fs.readdirSync('.');
-	console.log(files);
+const baseDir = 'x:/music';
+
+app.get('/hello', function(req, res) {
+	res.send("hello world!");
+});
+
+app.get('/fs/', function(req, res) {
+	var files = fs.readdirSync(baseDir);
+	res.send(files);
+});
+
+app.get('/fs/:path', function(req, res) {
+	var path = req.params.path;
+	path = baseDir + '/' + path;
+	var files = fs.readdirSync(path);
 	res.send(files);
 });
 
