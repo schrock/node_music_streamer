f34da28033a50c6307ae3977017e34246dfff32f
schrock <schrock@users.noreply.github.com>
2017-02-11 17:29:11 -0800

    better dir contents

---
+++ DirEntry.js
@@ -0,0 +1,10 @@
+module.exports = class DirEntry {
+
+	constructor(type, name, path, dirUrl) {
+		this.type = type;
+		this.name = name;
+		this.path = path;
+		this.dirUrl = dirUrl;
+	}
+
+}
--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-const fs = require('fs');
-const express = require('express');
-const app = express();
-
+// configuration
 var config = require('./config.json');
 
-app.get('/hello', function(req, res) {
+// 3rd party
+const app = require('express')();
+const fs = require('fs');
+
+// classes
+const DirEntry = require('./DirEntry.js');
+
+app.get('/hello', function (req, res) {
 	res.send("hello world!");
 });
 
-app.get('/dir', function(req, res) {
-	var path = req.query.path;
-	if (typeof path === 'undefined') {
-		res.send('You must specify query parameter \'path\'');
+app.get('/dir', function (req, res) {
+	var queryPath = req.query.path;
+	if (typeof queryPath === 'undefined') {
+		queryPath = '';
+	}
+	var realPath = config.baseDir + '/' + queryPath;
+
+	var dirContents;
+	try {
+		dirContents = fs.readdirSync(realPath);
+	} catch (Err) {
+		res.send('Unable to read path ' + realPath);
 		return;
 	}
-	path = config.baseDir + '/' + path;
-	var files = fs.readdirSync(path);
-	res.send(files);
+
+	var dirEntries = [];
+	for (var fileName of dirContents) {
+		var filePath = realPath + '/' + fileName;
+		var dirUrl = req.protocol + '://' + req.hostname + ':' + req.socket.localPort + '/dir?path=' + queryPath + '/' + fileName;
+		var stat = fs.statSync(filePath);
+		if (stat.isDirectory()) {
+			dirEntries.push(new DirEntry('dir', fileName, filePath, dirUrl));
+		} else if (stat.isFile()) {
+			dirEntries.push(new DirEntry('file', fileName, filePath, null));
+		}
+	}
+	res.send(dirEntries);
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
 });
-
